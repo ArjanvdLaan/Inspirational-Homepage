08e18451467b73cb34aa6f1bd79c3f20a84fd81d
Arjan van der Laan <laanvanderarjan@gmail.com>
2023-10-28 16:20:18 +0200

    add mock background + arrows

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // import { useState } from 'react'
 
 import "./App.css";
-import {InspirationalHomepage} from "./components/InspirationalHomepage/InspirationalHomepage";
+import { InspirationalHomepage } from "./components/InspirationalHomepage/InspirationalHomepage";
+import "../src/Background-CodeCademy-Homepage.jpg";
 
 function App() {
   return (
-    <>
+    <div className="container">
+      <i className="fas fa-chevron-left arrow-left" ></i>
+      <i className="fas fa-chevron-right arrow-right" ></i>
       <InspirationalHomepage />
-    </>
+    </div>
   );
 }
 
--- src/components/InspirationalHomepage/InspirationalHomepage.jsx
+++ src/components/InspirationalHomepage/InspirationalHomepage.jsx
@@ -3,15 +3,14 @@ import { WeatherBox } from "../WeatherBox/WeatherBox";
 import { GoalsBox } from "../GoalsBox/GoalsBox";
 import { QuoteBox } from "../QuoteBox/QuoteBox";
 
-export const InspirationalHomepage = () => {
 
-  
+export const InspirationalHomepage = () => {
   return (
-    <div className="inspirational-homepage">
+    <>
       <WeatherBox />
       <GoalsBox />
       <QuoteBox />
-    </div>
+    </>
   );
 };
 
